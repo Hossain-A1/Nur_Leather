dde606a9b8095a5182502f245491830d042185fe
Hossain-A1 <mrhossainahmed7@gmail.com>
2024-06-01 08:36:52 +0300

    project setup and navbar added

---
+++ app/components/Navbar.js
@@ -0,0 +1,72 @@
+import Link from "next/link";
+import { FaSearch } from "react-icons/fa";
+export default function Navbar() {
+  const navContents = [
+    { href: "/", label: "home" },
+    {
+      href: "/shop",
+      label: "shop",
+    },
+    {
+      href: "/products",
+      label: "products",
+    },
+    {
+      href: "/about",
+      label: "about",
+    },
+    {
+      href: "/contact",
+      label: "contact",
+    },
+  ];
+  return (
+    <header className='h-28  border-b max-md:hidden'>
+      <nav className='flex  container items-center gap-20 h-full w-full'>
+        <div className='logo-and-filter flex items-center justify-center gap-20'>
+          <div className='leading-7'>
+            <h2 className=''>Leather</h2>
+            <span className='block text-right uppercase font-semibold italic'>
+              Luxeris
+            </span>
+          </div>
+
+          <div className='filter flex gap-5 px-2.5 items-center justify-center border '>
+            <h3>=</h3>
+            <p>All Categories</p>
+          </div>
+        </div>
+
+        <div className='flex justify-center items-center gap-10'>
+          <ul className='lg:flex origin-top justify-center items-center max-lg:bg-white  duration-700  lg:gap-10  '>
+            {navContents.map((item) => (
+              <li
+                className='   flex justify-center font-semibold items-center text-[1rem]  capitalize'
+                key={item.label}
+              >
+                <Link href={item.href}>{item.label}</Link>
+              </li>
+            ))}
+          </ul>
+
+          <div className='input border relative  w-60'>
+            <input
+              type='search'
+              placeholder='search'
+              className='py-2 px-4 outline-none '
+            />
+
+            <span className='absolute right-0 h-full top-0 bottom-0   '>
+              <FaSearch className='text-xl bg-blue h-full  text-light' />
+            </span>
+          </div>
+
+          <div className='cart-and-user clear-start flex items-center'>
+            <span>cart</span>
+            <div>user</div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,24 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+
+      padding: {
+        DEFAULT: "2rem",
+      },
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        dark: "#333333",
+        light: "#FFFFFF",
+        blue: "#5356FF",
+        danger: "#E72929",
+        orange: "#FF7A3D",
+        red: "#B40003",
       },
     },
   },
