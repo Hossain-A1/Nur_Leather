74173478b6da158c14080d6a30435bd600326020
Hossain-A1 <mrhossainahmed7@gmail.com>
2024-07-28 18:57:04 +0300

    auth fn done

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -1,7 +1,100 @@
-import React from 'react'
+"use client";
+import { authLogic } from "@/lib/authLogic";
+import { login } from "@/redux/feature/auth/authSlice";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+import { useDispatch } from "react-redux";
 
 export default function LoginPage() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const handleAuthSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+      setLoading(true);
+      const data = await authLogic("/api/auth/login", formData);
+
+      if (data) {
+        setLoading(false);
+        setFormData({
+          email: "",
+          password: "",
+        });
+        dispatch(login(data));
+
+        router.push("/");
+      } else {
+        setLoading(false);
+      }
+    },
+    [formData, router, dispatch, setLoading]
+  );
   return (
-    <div>LoginPage Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias delectus dolores necessitatibus ipsam possimus esse, eaque nostrum blanditiis quia quis voluptatibus repellendus ea maxime nisi, corrupti earum tenetur doloremque ipsum numquam, architecto perferendis dolorem. In quae officiis deleniti voluptatibus eveniet corrupti incidunt, esse et dolorem. Voluptatem a officia dolorem doloribus nihil tenetur nemo tempora ex obcaecati iure, tempore quo, modi voluptas eveniet blanditiis, praesentium nam accusamus fugit incidunt adipisci laboriosam? Minima, similique voluptatum sint, saepe illo deleniti nesciunt odit corrupti totam dolorem doloribus excepturi repudiandae corporis reprehenderit. Nihil rerum non, sed vel officiis facere iure quod obcaecati velit illum ducimus, adipisci suscipit. Ratione eum perferendis omnis at culpa in quia, rem error et, vitae cumque esse temporibus assumenda voluptas corrupti, atque saepe possimus quaerat voluptatibus nesciunt. Id illum, commodi tempore consequuntur beatae enim sint maiores. Sequi, ut facere. Quo rem voluptas laborum repellendus tenetur aliquid veritatis ipsum iste neque distinctio ullam ut velit, fugiat recusandae quod consequuntur ad explicabo sit tempora nesciunt quasi pariatur nulla! Culpa laborum repellat quaerat ipsa quibusdam atque, sed veritatis beatae nam magnam aut blanditiis obcaecati earum adipisci fugit, incidunt rerum optio quidem? Quam cumque alias maxime! Inventore quia, blanditiis consequuntur facilis dicta, ipsam eveniet laborum ratione fugit natus impedit, doloribus omnis perspiciatis quae doloremque soluta. Dicta accusantium ratione reprehenderit nostrum magni delectus deleniti ducimus dolorum repellat nihil ex sint soluta odit aspernatur sapiente incidunt cupiditate laboriosam, rerum similique recusandae quae harum, enim amet? Blanditiis perferendis quam ad beatae quia praesentium nulla delectus nihil et corrupti ducimus, minima, quo omnis neque. Atque aspernatur, reiciendis adipisci magni, dolores iste expedita culpa similique illo veritatis nemo eaque vel dolorum dicta quae sed minima ipsam. A animi odio atque dolorem placeat doloremque neque facere ipsam autem ut, inventore similique quidem expedita optio ipsum quaerat voluptate corporis officiis cum necessitatibus itaque sapiente non! Accusantium sint hic commodi, in quidem reprehenderit quas facere, ea laboriosam iusto cum sunt aliquid labore iure placeat. Fuga iusto temporibus iste maxime non porro, veniam similique doloremque atque perferendis, quae in, pariatur minima! Doloremque, quos accusantium autem necessitatibus quod ipsum voluptatum accusamus tempore nesciunt atque asperiores. Adipisci sed corrupti, quibusdam incidunt quasi omnis veniam itaque, pariatur, eveniet suscipit quaerat nostrum quidem dicta cumque facere. Provident rerum ad qui iure dolorem. Quam corrupti, amet distinctio dolorum similique fugiat voluptates hic quasi expedita commodi quod recusandae molestiae eum culpa eveniet, qui voluptas nulla veniam mollitia dolorem quidem? Iusto!</div>
-  )
+    <div className='container sp mt-10 flex flex-col items-center justify-center '>
+      <form
+        onSubmit={handleAuthSubmit}
+        className='flex flex-col gap-3 lg:w-2/3 w-full'
+      >
+        <h2>Login to your account</h2>
+
+        <div className='flex flex-col gap-1'>
+          <label htmlFor='email' className=' text-lg'>
+            Email:
+          </label>
+          <input
+            required
+            type='email'
+            id='email'
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+            placeholder='Email'
+            className='border-none py-3 px-6 rounded eq focus:outline-blue bg-dark/10'
+          />
+        </div>
+        <div className='flex flex-col gap-1'>
+          <label htmlFor='password' className=' text-lg'>
+            Password:
+          </label>
+          <input
+            required
+            type='password'
+            id='password'
+            value={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+            placeholder='Password'
+            className='border-none py-3 px-6 rounded eq focus:outline-blue bg-dark/10'
+          />
+        </div>
+
+        <div>
+          <button
+            type='submit'
+            className='border-none py-3 px-6 rounded eq bg-blue text-light '
+          >
+            Login
+          </button>
+        </div>
+
+        <div>
+          <span className='text-sm'>Do not have an account?</span>
+          <Link href='/auth/login' className='text-green'>
+            {" "}
+            Register
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
--- app/auth/register/page.js
+++ app/auth/register/page.js
@@ -1,17 +1,65 @@
+"use client";
+import { authLogic } from "@/lib/authLogic";
+import { login } from "@/redux/feature/auth/authSlice";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+import { useDispatch } from "react-redux";
 
-export default function UserPage() {
+export default function RegisterPage() {
+  const [formData, setFormData] = useState({
+    name: "",
+    address: "",
+    phoneNo: "",
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const handleAuthSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+      setLoading(true);
+      const data = await authLogic("/api/auth/register", formData);
+
+      if (data) {
+        setLoading(false);
+        setFormData({
+          name: "",
+          address: "",
+          phoneNo: "",
+          email: "",
+          password: "",
+        });
+        dispatch(login(data));
+
+        router.push("/");
+      } else {
+        setLoading(false);
+      }
+    },
+    [formData, router, dispatch, setLoading]
+  );
   return (
     <div className='container sp mt-10 flex flex-col items-center justify-center '>
-      <form className='flex flex-col gap-3 lg:w-2/3 w-full'>
+      <form
+        onSubmit={handleAuthSubmit}
+        className='flex flex-col gap-3 lg:w-2/3 w-full'
+      >
         <h2>Create a new account</h2>
         <div className='flex flex-col gap-1'>
           <label htmlFor='name' className=' text-lg'>
             Name:
           </label>
           <input
+            required
             type='text'
             id='name'
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
             placeholder='Name'
             className='border-none py-3 px-6 rounded eq focus:outline-blue bg-dark/10'
           />
@@ -21,8 +69,13 @@ export default function UserPage() {
             Address:
           </label>
           <input
+            required
             type='text'
             id='address'
+            value={formData.address}
+            onChange={(e) =>
+              setFormData({ ...formData, address: e.target.value })
+            }
             placeholder='Address'
             className='border-none py-3 px-6 rounded eq focus:outline-blue bg-dark/10'
           />
@@ -32,8 +85,13 @@ export default function UserPage() {
             Phone No:
           </label>
           <input
+            required
             type='tel'
             id='phone'
+            value={formData.phoneNo}
+            onChange={(e) =>
+              setFormData({ ...formData, phoneNo: e.target.value })
+            }
             placeholder='Phone'
             className='border-none py-3 px-6 rounded eq focus:outline-blue bg-dark/10'
           />
@@ -43,8 +101,13 @@ export default function UserPage() {
             Email:
           </label>
           <input
+            required
             type='email'
             id='email'
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
             placeholder='Email'
             className='border-none py-3 px-6 rounded eq focus:outline-blue bg-dark/10'
           />
@@ -54,8 +117,13 @@ export default function UserPage() {
             Password:
           </label>
           <input
+            required
             type='password'
             id='password'
+            value={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
             placeholder='Password'
             className='border-none py-3 px-6 rounded eq focus:outline-blue bg-dark/10'
           />
